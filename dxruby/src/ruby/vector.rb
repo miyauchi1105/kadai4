73267339b910e34f48a627f5c591bee15f437d77
e1336 <e1336@PC4-31.cc.matsue-ct.jp>
2017-09-26 14:21:14 +0900

    課題４最終

--- dxruby/src/ruby/vector.rb
+++ dxruby/src/ruby/vector.rb
@@ -1,14 +1,23 @@
 class Vector
+  attr_accessor :x, :y
+
+
   def initialize(x, y)
     @x = x
     @y = y
   end
 
+  def add(v)
+    ans = Vector.new(v.x+@x,v.y+@y)
+    return ans
+  end
+
   def to_s
-    "(#{@x}, #{@y})"
+    return"(#{@x}, #{@y})"
   end
 
   def length
-    Math.sqrt(@x**2 + @y**2)
+    ans = @x*@x+@y*@y
+    return Math.sqrt(@x**2 + @y**2)
   end
 end
